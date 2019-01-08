324a3fb491ae80c40cf5bcdd6cd1325207366ec7
牧羊人 <l>
2019-01-08 13:46:38 +0800

    更新

--- public/admin.php
+++ public/admin.php
@@ -18,7 +18,7 @@ define('APP_PATH', __DIR__ . '/../application/');
 define('CONF_PATH', __DIR__ . '/../config/');
 
 // 绑定模块
-define('BIND_MODULE','admin');
+define('BIND_MODULE', 'admin');
 
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
